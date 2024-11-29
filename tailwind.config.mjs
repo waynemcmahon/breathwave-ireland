d45456834d13981c0b78370accc1eec8f0d0640c
Wayne <waynemcmahon22@gmail.com>
2024-11-29 20:31:08 +0000

    updates

---
+++ tailwind.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      animation: {
+        pulse: 'pulse 10s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+      },
+    },
+  },
+  plugins: [],
+}
